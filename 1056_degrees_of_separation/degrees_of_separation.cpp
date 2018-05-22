1e231ec67202e8f813681e40473766aee3b75a52
GabrielXia <Ga.Xiajin@gmail.com>
2018-05-22 18:34:29 +0200

    w3q1

--- 1056_degrees_of_separation/degrees_of_separation.cpp
+++ 1056_degrees_of_separation/degrees_of_separation.cpp
@@ -17,33 +17,35 @@ using namespace std;
 
 
 int BFS(int root, bool path[MAXNODE][MAXNODE], int nodeNum) {
-  queue<int> Q;
-  bool visited[MAXNODE] = {};
-  memset(visited, false, sizeof(visited[0]*MAXNODE));
-  int distance[MAXNODE] = {};
-  memset(distance, 0, sizeof(distance[0]*MAXNODE));
-  Q.push(root);
-  while (!Q.empty()) {
-    int u = Q.front();
-    Q.pop();
-    if (visited[u]) continue;
-    visited[u] = true;
-    for (int i=0; i<nodeNum; i++) {
-      if (path[u][i]) {
-        distance[i] = distance[u] +1;
-        Q.push(i);
-      }
+    queue<int> Q;
+    bool visited[MAXNODE] = {};
+    memset(visited, false, sizeof(visited[0]*MAXNODE));
+    int distance[MAXNODE] = {};
+    memset(distance, 0, sizeof(distance[0]*MAXNODE));
+    Q.push(root);
+    while (!Q.empty()) {
+        int u = Q.front();
+        Q.pop();
+        if (visited[u]) continue;
+        visited[u] = true;
+        for (int i=0; i<nodeNum; i++) {
+            if (path[u][i]) {
+                Q.push(i);
+                if (i!= root && distance[i]==0) {
+                    distance[i] = distance[u] +1;
+                }
+            }
+        }
     }
-  }
 
-  int maxValue = 0;
-  for (int i = 0; i < nodeNum; i++) {
-    if(!visited[i]) return -1;
-    if (distance[i] > maxValue) {
-      maxValue = distance[i];
+    int maxValue = 0;
+    for (int i = 0; i < nodeNum; i++) {
+        if(!visited[i]) return -1;
+        if (distance[i] > maxValue) {
+            maxValue = distance[i];
+        }
     }
-  }
-  return maxValue;
+    return maxValue;
 }
 
 int main() {
@@ -51,49 +53,49 @@ int main() {
     bool path[MAXNODE][MAXNODE];
     int netWork = 0;
     while (scanf("%d %d", &p, &r) == 2) {
-      if (p == 0 && r == 0) {
-        break;
-      }
-      netWork ++;
-
-      // read
-      map<string, int> record = {};
-      memset(path, false, sizeof(path[0][0])*MAXNODE*MAXNODE);
-      int index = 0;
-      for (int i = 0; i < r; i++) {
-        char name1[50], name2[50];
-        scanf("%s %s", name1, name2);
-        if (record.find(name1) == record.end()) {
-          record.insert(pair<string, int>(name1, index++));
+        if (p == 0 && r == 0) {
+            break;
         }
-        if (record.find(name2) == record.end()) {
-          record.insert(pair<string, int>(name2, index++));
+        netWork ++;
+
+        // read
+        map<string, int> record = {};
+        memset(path, false, sizeof(path[0][0])*MAXNODE*MAXNODE);
+        int index = 0;
+        for (int i = 0; i < r; i++) {
+            char name1[50], name2[50];
+            scanf("%s %s", name1, name2);
+            if (record.find(name1) == record.end()) {
+                record.insert(pair<string, int>(name1, index++));
+            }
+            if (record.find(name2) == record.end()) {
+                record.insert(pair<string, int>(name2, index++));
+            }
+            int pos1 = record.find(name1)-> second;
+            int pos2 = record.find(name2) -> second;
+            path[pos1][pos2] = true;
+            path[pos2][pos1] = true;
         }
-        int pos1 = record.find(name1)-> second;
-        int pos2 = record.find(name2) -> second;
-        path[pos1][pos2] = true;
-        path[pos2][pos1] = true;
-      }
 
-      if(record.size() < p) {
-        printf("Network %d: DISCONNECTED\n\n", netWork);
-      }
-
-      int result = 0;
-      for (int i = 0; i < p; i++) {
-        int maxDepth = BFS(i, path, p);
-        if (maxDepth == -1) {
-          printf("Network %d: DISCONNECTED\n\n", netWork);
-          break;
+        if(record.size() < p) {
+            printf("Network %d: DISCONNECTED\n\n", netWork);
         } else {
-          if(result < maxDepth) {
-            result = maxDepth;
-          }
-        }
+          int result = 0;
+          for (int i = 0; i < p; i++) {
+              int maxDepth = BFS(i, path, p);
+              if (maxDepth == -1) {
+                  printf("Network %d: DISCONNECTED\n\n", netWork);
+                  break;
+              } else {
+                  if(result < maxDepth) {
+                      result = maxDepth;
+                  }
+              }
 
-        if (i == p-1) {
-          printf("Network %d: %d\n\n", netWork, result);
-        }
+              if (i == p-1) {
+                  printf("Network %d: %d\n\n", netWork, result);
+              }
+          }
       }
     }
 
